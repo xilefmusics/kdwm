c99c41bb9bf17c8c1c7bf852b4c61a2d70973330
xilefmusics <xilefmusics@gmail.com>
2019-09-27 11:28:30 +0200

    removed killX

--- config.h
+++ config.h
@@ -6,5 +6,4 @@ static wm_keybinding_t wm_keybindings[] = {
     {MODKEY|ShiftMask, XK_q, wm_stop, NONE, NULL},
     {MODKEY, XK_Return, system, STRING, "st &"},
     {MODKEY, XK_r, system, STRING, "dmenu_run &"},
-    {MODKEY|ShiftMask|ControlMask, XK_q, system, STRING, "kill -2 $(ps ax | grep /usr/lib/Xorg | grep tty1 | awk '{split($0,a," "); print a[1]}')"},
 };
