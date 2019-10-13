20e778db972e078ba52291ce09a92c32af74d712
leonMatzner <leonmatzner@aol.com>
2019-10-13 22:42:19 +0200

    added state logging

--- modules/logger.c
+++ modules/logger.c
@@ -25,4 +25,22 @@ void logsi(char *s, int i) {
 }
 
 void log_state() {
+    // init logging
+    char path_buffer[256];
+    char *homedir = getenv("HOME");
+    strcpy(path_buffer, homedir);
+    strcat(path_buffer, "/.kdwm/state.txt");
+    FILE *fp = fopen(path_buffer, "a");
+    // log
+    fprintf(fp, "state:\n");
+    wm_client_t *client = wm_global.client_list.head_client;
+    while (client->next != NULL) {
+        fprintf(fp, "client window-ID %d, tag-mask (in hex) %x; ", client->window, client->tag_mask);
+        client = client->next;
+    }
+    if (client!=NULL) {
+        fprintf(fp, "client window-ID %d, tag-mask (in hex) %x; \n", client->window, client->tag_mask);
+    }
+    // close logging
+    fclose(fp);
 }
